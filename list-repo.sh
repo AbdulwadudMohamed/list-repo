5ea0fe2c78ed4b7685f974e9940334df185679e8
abdulwadud <abdilahi.j.m@gmail.com>
2023-01-27 12:06:20 +0000

    Issue #1 solved: Username input+Verification

--- list-repo.sh
+++ list-repo.sh
@@ -1,15 +1,34 @@
 #!/bin/sh
 
-# Set your GitHub username
-username="AbdulwadudMohamed"
+#Get Github Username
+GETUSERNAME() {
+    echo "Enter GitHub Username (Case sensitive)"
+    read username
+    VERIFYUSERNAME username
+}
 
-# Use the GitHub API to get a list of your repositories
+#Verify username
+VERIFYUSERNAME() {
+read -p "Is your username: $username?" yn
+    case $yn in 
+        [Yy]* ) echo "SUCCESS";;
+        [Nn]* ) echo "Wrong Details Recorded"GETUSERNAME;; 
+        *) echo "Please answer yes or no.";;
+        esac
+}
+
+GETUSERNAME
+
+#Use the GitHub API to get: 
+
+#Repositories
 repos=$(curl -s "https://api.github.com/users/$username/repos?per_page=1000")
 
-# Loop through the list of repositories
+#Loop through the list of repositories
 for repo in $(echo "${repos}" | jq -r '.[] | @base64'); do
     repo_name=$(echo ${repo} | base64 --decode | jq -r '.name')
     repo_desc=$(echo ${repo} | base64 --decode | jq -r '.description')
+    
     echo "$repo_name - $repo_desc"
 done
 
