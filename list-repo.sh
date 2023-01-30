83f46b05c13d8e6348207f3e2477b2029d5392c5
abdulwadud <abdilahi.j.m@gmail.com>
2023-01-30 11:18:14 +0000

    Added Access Token to curl + improved the cmd

--- list-repo.sh
+++ list-repo.sh
@@ -1,6 +1,6 @@
 #dont need (#!/bin/sh)?
-#Declaring Variables
 
+#Declaring Variables
 #Username
 username="AbdulwadudMohamed"
 #Stored Reponame Variables
@@ -20,59 +20,67 @@ Rc6=1
 #Counter Variable
 n=1
 
-#Repositories
-repos=$(curl -s "https://api.github.com/users/$username/repos?per_page=1000")
+#Repositories changed to 100 per page instead of 1000
+repos=$(curl \
+-H "Authorization: Bearer $TOKEN" \
+-s "https://api.github.com/users/$username/repos?per_page=100")
 
 #Function to store Repo names and commits - currently not optimised
 Store() {
     #names
-    if (( $n == 1 )); then
+    if [ $n = 1 ]; then
         Rn1=$1
         n=$(($n+1))
+        #echo "$Rn1"
     fi
-    if (( $n == 2 )); then
+    if [ $n == 2 ]; then
         Rn2=$1
         n=$(($n+1))
+        #echo "$Rn2"
     fi
-    if (( $n == 3 )); then
+    if [ $n == 3 ]; then
         Rn3=$1
         n=$(($n+1))
+        #echo "$Rn3"
     fi
-    if (( $n == 4 )); then
+    if [ $n == 4 ]; then
         Rn4=$1
         n=$(($n+1))
+        #echo "$Rn4"
     fi
-    if (( $n == 5 )); then
+    if [ $n == 5 ]; then
         Rn5=$1
         n=$(($n+1))
+        #echo "$Rn5"
     fi
-    if (( $n == 6 )); then
+    if [ $n == 6 ]; then
         Rn6=$1
         n=$(($n+1))
+        #echo "$Rn6"
     fi
 
     #commits
-    if (( $n == 1 )); then
+    if [ $n == 1 ]; then
         Rc1=$2
         n=$(($n+1))
     fi
-    if (( $n == 2 )); then
+    if [ $n == 2 ]; then
         Rc2=$2
         n=$(($n+1))
     fi
-    if (( $n == 3 )); then
+    if [ $n == 3 ]; then
         Rc3=$2
         n=$(($n+1))
     fi
-    if (( $n == 4 )); then
+    if [ $n == 4 ]; then
         Rc4=$2
         n=$(($n+1))
     fi
-    if (( $n == 5 )); then
+    if [ $n == 5 ]; then
         Rc5=$2
         n=$(($n+1))
     fi
-    if (( $n == 6 )); then
+    if [ $n == 6 ]; then
         Rc6=$2
         n=$(($n+1))
     fi
@@ -83,19 +91,17 @@ for repo in $(echo "${repos}" | jq -r '.[] | @base64'); do
     repo_name=$(echo ${repo} | base64 --decode | jq -r '.name')
     repo_desc=$(echo ${repo} | base64 --decode | jq -r '.description')
     commits=$(curl -s -k "https://api.github.com/repos/$username/$repo_name/commits?per_page=1" | sed -n '/^[Ll]ink:/ s/.*"next".*page=\([0-9]*\).*"last".*/\1/p')
-   
+    commits=$(curl \
+    -H "Authorization: Bearer $TOKEN" \
+    -s https://api.github.com/repos/$username/$repo_name/commits?per_page=100 | jq '. | length')
+    
     #Output
-    #echo "Collected Data"
-    #echo "Repo Name: $repo_name" 
-    #echo "Repo Desc: $repo_desc"
-    #echo "No of commits: $commits"
+    echo "Collected Data"
+    echo "Repo Name: $repo_name" 
+    echo "Repo Desc: $repo_desc"
+    echo "No of commits: $commits"
     
     #Call Store function to capture data
-    Store repo_name commits
-
+    Store $repo_name $commits
     
 done
-
-#test stored values
-echo "$Rn1"
-echo "$Rc1"
