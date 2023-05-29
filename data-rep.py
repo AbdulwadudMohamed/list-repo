f64b506efb223e3a6923457ae9f3cee18ec238a4
abdulwadud <abdilahi.j.m@gmail.com>
2023-05-29 18:40:17 +0100

    Formatted code

--- data-rep.py
+++ data-rep.py
@@ -1,43 +1,43 @@
-#Call & run scripts
-#import subprocess
-#Data representation
+# Call & run scripts
+# import subprocess
+# Data representation
 import matplotlib.pyplot as plt
 
-#result = subprocess.run(['Bash', './list-repo.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+# result = subprocess.run(['Bash', './list-repo.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
 
-#if result.returncode == 0:
+# if result.returncode == 0:
 #    print("(Return code) Script completed successfully.")
-#else:
+# else:
 #    print("(Return code) Script failed with error code", result.returncode)
 
-#Read and store Repo names
+# Read and store Repo names
 repo_name_array = []
 with open("REPONAMES.txt", "r") as f:
     for line in f:
         repo_name_array.append(line.strip())
 
-#Read and store Repo commits as ints
-repo_commit_array=[]
+# Read and store Repo commits as ints
+repo_commit_array = []
 with open("REPOCOMMITS.txt", "r") as g:
     for line in g:
         repo_commit_array.append(int(line.strip()))
 
-#Create a new figure and axes
+# Create a new figure and axes
 fig, ax = plt.subplots()
-fig.subplots_adjust(bottom=0.3) # increase bottom parameter to 0.2
-fig.set_size_inches(10,5) # sets the size of the figure
+fig.subplots_adjust(bottom=0.3)  # increase bottom parameter to 0.2
+fig.set_size_inches(10, 5)  # sets the size of the figure
 
-#Create the bar chart
+# Create the bar chart
 ax.bar(range(len(repo_commit_array)), repo_commit_array)
 
-#Set the x-axis labels
-ax.set_xlabel('Repository names')
+# Set the x-axis labels
+ax.set_xlabel("Repository names")
 ax.set_xticks(range(len(repo_commit_array)))
 ax.set_xticklabels(repo_name_array, rotation=55, fontsize=7)
 
-#Set the y-axis label and limit
-ax.set_ylabel('Commits')
-ax.set_ylim(bottom=0, top=max(repo_commit_array)+10)
+# Set the y-axis label and limit
+ax.set_ylabel("Commits")
+ax.set_ylim(bottom=0, top=max(repo_commit_array) + 10)
 
-#Show the plot
+# Show the plot
 plt.show()
--- list-repo.sh
+++ list-repo.sh
@@ -7,7 +7,7 @@ username="AbdulwadudMohamed"
 REPONAMES=()
 REPOCOMMITS=()
 
--H "Authorization: Bearer $TOKEN" \
+-H "Authorization: Bearer $TOKEN"
 
 #Repositories changed to 100 per page instead of 1000
 repos=$(curl \
@@ -19,30 +19,29 @@ for repo in $(echo "${repos}" | jq -r '.[] | @base64'); do
     repo_name=$(echo ${repo} | base64 --decode | jq -r '.name')
     repo_desc=$(echo ${repo} | base64 --decode | jq -r '.description')
     commits=$(curl \
-    -H "Authorization: Bearer $TOKEN" \
-    -s https://api.github.com/repos/$username/$repo_name/commits?per_page=100 | jq '. | length')
-    
+        -H "Authorization: Bearer $TOKEN" \
+        -s https://api.github.com/repos/$username/$repo_name/commits?per_page=100 | jq '. | length')
+
     #Store data into list
     REPONAMES+=("$repo_name")
     REPOCOMMITS+=("$commits")
-    
+
     #Output
     echo "Collected Data"
-    echo "Repo Name: $repo_name" 
+    echo "Repo Name: $repo_name"
     echo "Repo Desc: $repo_desc"
     echo "No of commits: $commits"
 done
 
 #Loop through the array and write each element to the file
-echo -n > REPONAMES.txt
+echo -n >REPONAMES.txt
 for repo in "${REPONAMES[@]}"; do
-    echo $repo >> REPONAMES.txt
+    echo $repo >>REPONAMES.txt
 done
 
-echo -n > REPOCOMMITS.txt
+echo -n >REPOCOMMITS.txt
 for repo in "${REPOCOMMITS[@]}"; do
-    echo $repo >> REPOCOMMITS.txt
+    echo $repo >>REPOCOMMITS.txt
 done
-
 
 echo "End of Bash script"
